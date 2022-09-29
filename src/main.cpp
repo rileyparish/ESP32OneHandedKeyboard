9a9ab38f27ebe39ede307adbedc0ba2622cfdf05
Riley Parish <parish.riley@gmail.com>
2022-09-28 19:59:13 -0600

    Refactored to work with ESP32 instead of Arduino.

--- src/main.cpp
+++ src/main.cpp
@@ -7,29 +7,28 @@ Thanks, and have fun! :)
 */
 
 #include <Arduino.h>
-#include <Keyboard.h>
+#include <BleKeyboard.h>
 #include <math.h>
 
 class FingerInput {       
     public:             
-        uint8_t analogPin;  // the analog pin that is assigned to this finger     
+        uint8_t capacitivePin;  // the GPIO pin that is assigned to this finger     
         int fingerValue;    // the binary value assigned to this finger
-        bool isActive = false;      // whether electricity is flowing to the pin or not
 };
+// this is the bluetooth keyboard
+BleKeyboard bleKeyboard;
 
-// since this script takes control of the keyboard, I want to be able to disable it. The start pin must read LOW before the Arduino can control the keyboard.
-const int startPin = 13;
+// since this script takes control of the keyboard, I want to be able to disable it. The start pin must read LOW before the ESP32 can control the keyboard.
+const int START_PIN = 26;       // D26 as labeled on the board
 
-// these are the physical GPIO pins on the Arduino Micro
+// these are the physical GPIO pins on the ESP32
 const int NUM_INPUTS = 5;
-uint8_t fingerPins[NUM_INPUTS] = {A5, A4, A3, A2, A1};
+uint8_t fingerPins[NUM_INPUTS] = {T5, T6, T7, T8, T9};  // corresponds to pins D12, D14, D27, D32, and D33 respectively
 
-// the resistance thresholds when using 22M ohm resistors
-const int ACTIVE_THRESHOLD_MAX = 200; // this is the maximum reading when the button is pressed. Any reading underneath this value will register as a touch (a higher number will allow inputs with a higher natural resistance at the risk of false positives)
-const int INACTIVE_THRESHOLD_MIN = 300; // this is the minimum reading when there is no connection. Any reading higher than this will register as no longer touching
+const int ACTIVE_THRESHOLD = 25; // this is the maximum reading when the button is pressed. Any reading below this value will register as a touch.
 
 // the amount of time to wait before sending/stopping keyboard signals
-// a longer value will reduce responsiveness but be more forgiving of mistakes
+// a longer value will reduce responsiveness but will be more forgiving of imperfectly synced actions
 const unsigned long IO_DELAY = 100;
 // this stores what the last keypress was so I know when the input changes
 int previousKeySum = 0;
@@ -38,14 +37,14 @@ int currentKeySum = 0;
 
 char baseLetters[32] = {'*', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x','y', 'z', 
     '-' /*Alt letter mode*/, 
-    '-', 
+    '-' /* Special keys mode */, 
     '-', 
     (char)0xB2 /* BKSP */, 
     (char)0x20 /* SPACE */
 };
 char altSymbols[32] = {'*', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '{', '}', ';', ':', '.', '?', 
     '-' /*Alt letter mode*/, 
-    '-', 
+    '-' /* Special keys mode */, 
     '-', 
     (char)0xB2 /* BKSP */, 
     (char)0x20 /* SPACE */
@@ -60,7 +59,7 @@ FingerInput fingerInputs[NUM_INPUTS];
 int getCurrentKeySum(){
     currentKeySum = 0;
     for(int i = 0; i < NUM_INPUTS; i++){
-        if(analogRead(fingerInputs[i].analogPin) < ACTIVE_THRESHOLD_MAX){
+        if(touchRead(fingerInputs[i].capacitivePin) < ACTIVE_THRESHOLD){
             currentKeySum += fingerInputs[i].fingerValue;
         }
     }
@@ -81,11 +80,12 @@ char convertSumToChar(int sum){
 }
 
 void setup() {
-    Serial.begin(9600);
-    // do nothing until I've confirmed that I want to give arduino control of the keyboard
-    pinMode(startPin, INPUT_PULLUP);
-    while(digitalRead(startPin) == HIGH){
-        Serial.println("Waiting for startPin before continuing...");
+    Serial.begin(115200);
+    bleKeyboard.begin();
+    // don't proceed until I've confirmed that I want to give ESP32 control of the keyboard
+    pinMode(START_PIN, INPUT_PULLUP);
+    while(digitalRead(START_PIN) == HIGH){
+        Serial.println("Waiting for START_PIN before continuing...");
         delay(500);
     }
 
@@ -96,33 +96,36 @@ void setup() {
 
     // init each of the fingerInput objects
     for(int i = 0; i < NUM_INPUTS; i++){
-        fingerInputs[i].analogPin = fingerPins[i];
+        fingerInputs[i].capacitivePin = fingerPins[i];
         fingerInputs[i].fingerValue = round(pow(2, i));    // each finger gets assigned an increasing power of 2. round() because pow() works with doubles
     }
 }
 
 void loop() {
-    // continuously check each of the finger pins to detect when a change in configuration occurs
-    if(getCurrentKeySum() != previousKeySum){
-        // then an event has started to occur. Wait for other inputs to settle (because human actions are very slow for computers)
-        delay(IO_DELAY);    // TODO: this causes problems if a key is pressed and released before 100 ms has elapsed
-        currentKeySum = getCurrentKeySum();
-        // first determine if the combination is activating a different mode
-        if(currentKeySum == ALT_SYMBOL_MODE){
-            // toggle alt symbol mode
-            altSymbolMode = !altSymbolMode;
-        }else{
-            // if a special mode was not provided, process the input as a keystroke
-            if(previousKeySum == 0 && currentKeySum != 0){
-                // then no keys are being pressed and we want to press a key
-                Keyboard.press(convertSumToChar(currentKeySum));
-                previousKeySum = currentKeySum;
+    // no need to do anything if the keyboard is not currently connected to a device
+    if(bleKeyboard.isConnected()){
+        // continuously check each of the finger pins to detect when a change in configuration occurs
+        if(getCurrentKeySum() != previousKeySum){
+            // an event has started to occur. Wait for other inputs to settle (because human actions are very slow for computers)
+            delay(IO_DELAY);    // TODO: no touch will register if a key is pressed and released within IO_DELAY. Could use millis() instead.
+            currentKeySum = getCurrentKeySum();
+            // first determine if the combination is activating a different keyboard mode
+            if(currentKeySum == ALT_SYMBOL_MODE){
+                // toggle alt symbol mode
+                altSymbolMode = !altSymbolMode;
             }else{
-                // then we want to release a key
-                // this will probably need to change when the keyboard gets more complex
-                Keyboard.releaseAll();
-                // no keys are being pressed, so reset previousKeySum
-                previousKeySum = 0;
+                // if a special mode was not provided, process the input as a keystroke
+                if(previousKeySum == 0 && currentKeySum != 0){
+                    // then no keys are currently being pressed and we want to press a key
+                    bleKeyboard.press(convertSumToChar(currentKeySum));
+                    previousKeySum = currentKeySum;
+                }else{
+                    // if previousKeySum is set, then we want to release whatever was being pressed
+                    // this will probably need to change when the keyboard gets more complex
+                    bleKeyboard.releaseAll();
+                    // no keys are being pressed anymore, so reset previousKeySum
+                    previousKeySum = 0;
+                }
             }
         }
     }
